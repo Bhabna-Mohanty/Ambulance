2b4328905f824abf0d4313f5ea33ced4d9d87412
Bhabna-Mohanty <bhabnamohanty04@gmail.com>
2025-05-10 20:00:04 +0530

    captain login and profile

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const captainController = require('../controllers/captain.controller.js');
-
+const authmiddleware = require('../middlewares/auth.middleware.js').authCaptain;
 
 
 router.post('/register', [
@@ -16,6 +16,15 @@ router.post('/register', [
     captainController.registerCaptain
 )
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+],
+    captainController.loginCaptain
+)
+router.get('/profile',authmiddleware.authCaptain ,captainController.getCaptainProfile);
+
+router.get('/logout', authmiddleware.authCaptain, captainController.logoutCaptain);
 
 
 module.exports = router;
